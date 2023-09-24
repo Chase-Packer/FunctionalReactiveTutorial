298a19dca1aa30fb202ca0998d5d601b66807279
Chase Packer <packerchase@hotmail.com>
2023-09-24 15:21:24 -0400

    Created initial demo file for functional

--- Code/FunctionalReactiveTutorial.py
+++ Code/FunctionalReactiveTutorial.py
@@ -1,9 +1,26 @@
-import qt
+from PyQt6.QtWidgets import QApplication, QWidget
 import reactivex as rx
+import sys
+
+#Provide the pseudocode
 
 
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle("Functional & Reactive Programming Tutorial")
+window.show()
 
 
+app.exec()
 
 
+#Pre-manufactured stream
+
+#Use Reactive Programming to handle the stream
+
+#Filtering the Stream
+
+
+#Using Functional Programming to perform functions on the steam
 
